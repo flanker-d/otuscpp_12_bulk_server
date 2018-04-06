a3d9ff480f771f132f220922e2b59d64c916bf65
Artyom Sorokoumov <artyom.sorokoumov@gmail.com>
2018-04-06 18:38:49 +0300

    async_read_until_eol changed to async_read_some_eol

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
     int file_loggers_count = 2;
 
     common::tcp_server_params_t params;
-    params.do_read_type = common::read_func_type_e::read_until_eol;
+    params.do_read_type = common::read_func_type_e::async_read_some_eol;
     params.use_strand = false;
 
     async::bulk_server server(io_service, port, bulk_size, file_loggers_count, params);
